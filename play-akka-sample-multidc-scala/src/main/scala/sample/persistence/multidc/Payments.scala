dc423c6785a31dae0c6129e487cb5910e80746f0
Paul Dale <paul.dale@lightbend.com>
2019-12-10 23:25:38 +0000

    add balance to SuccessfulEvent

--- play-akka-sample-multidc-scala/src/main/scala/sample/persistence/multidc/Payments.scala
+++ play-akka-sample-multidc-scala/src/main/scala/sample/persistence/multidc/Payments.scala
@@ -27,28 +27,38 @@ package payments.model
 
   sealed trait Event extends Correlated
   sealed trait FailedEvent extends Event
-  sealed trait SuccessfulEvent extends Event
+  sealed trait SuccessfulEvent extends Event {
+    def balance:Balance
+  }
   sealed trait RaceConditionEvent extends Event
 
   case class State(history:List[Event])
 
 
   case class AuthorizationRequested (id: IdempotentIdentifier, amount: BigDecimal) extends Event
-  case class AuthorizationSuccessful(id: IdempotentIdentifier, authorization: Authorization) extends SuccessfulEvent
+  case class AuthorizationSuccessful(id: IdempotentIdentifier, authorization: Authorization) extends SuccessfulEvent {
+    val balance:Balance = authorization
+  }
   case class AuthorizationFailed    (id: IdempotentIdentifier, message: String) extends FailedEvent
   case class AuthorizationTimeout   (id: IdempotentIdentifier, message: String) extends FailedEvent
 
   case class SettlementRequested (id: IdempotentIdentifier, amount: BigDecimal) extends Event
-  case class SettlementSuccessful(id: IdempotentIdentifier, settlement: Settlement)  extends SuccessfulEvent
+  case class SettlementSuccessful(id: IdempotentIdentifier, settlement: Settlement)  extends SuccessfulEvent {
+    val balance:Balance = settlement
+  }
   case class SettlementFailed    (id: IdempotentIdentifier, message: String) extends FailedEvent
   case class SettlementTimeout   (id: IdempotentIdentifier, message: String) extends FailedEvent
 
   case class RefundRequested (id: IdempotentIdentifier, amount: BigDecimal) extends Event
-  case class RefundSuccessful(id: IdempotentIdentifier, refund: Refund)  extends SuccessfulEvent
+  case class RefundSuccessful(id: IdempotentIdentifier, refund: Refund)  extends SuccessfulEvent {
+    val balance:Balance = refund
+  }
   case class RefundFailed    (id: IdempotentIdentifier, message: String) extends FailedEvent
   case class RefundTimeout   (id: IdempotentIdentifier, message: String) extends FailedEvent
 
   case class ChargebackRequested (id: IdempotentIdentifier, amount: BigDecimal) extends Event
-  case class ChargebackSuccessful(id: IdempotentIdentifier, chargeback: Chargeback)  extends SuccessfulEvent
+  case class ChargebackSuccessful(id: IdempotentIdentifier, chargeback: Chargeback)  extends SuccessfulEvent {
+    val balance:Balance = chargeback
+  }
   case class ChargebackFailed    (id: IdempotentIdentifier, message: String) extends FailedEvent
   case class ChargebackTimeout   (id: IdempotentIdentifier, message: String) extends FailedEvent

88599f4bb926a1552edb78c8c94ae65b60e5c357
Paul Dale <paul.dale@lightbend.com>
2019-12-11 14:00:36 +0000

    protobuf scaffolding.

---
+++ play-akka-sample-multidc-scala/src/main/scala/sample/model/PaymentLifecycleSerializer.scala
@@ -0,0 +1,22 @@
+package sample.model
+
+import java.io.NotSerializableException
+
+import akka.serialization.BaseSerializer
+import akka.serialization.SerializerWithStringManifest
+import sample.model.protobuf.LifecycleTrackingMessages
+import scala.collection.JavaConverters._
+
+class PaymentLifecycleSerializer(val system: akka.actor.ExtendedActorSystem)
+  extends SerializerWithStringManifest with BaseSerializer {
+
+  override def manifest(o: AnyRef): String = o match {
+    case _ ⇒
+      throw new IllegalArgumentException(s"Can't serialize object of type ${o.getClass} in [${getClass.getName}]")
+  }
+
+  override def toBinary(o: AnyRef): Array[Byte] = o match {
+    case _ ⇒
+      throw new IllegalArgumentException(s"Cannot serialize object of type [${o.getClass.getName}]")
+  }
+

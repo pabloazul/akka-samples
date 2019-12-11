57a8615e99bb0f3a0f1e449f61d605c2d08a63d0
Paul Dale <paul.dale@lightbend.com>
2019-12-11 13:52:48 +0000

    Simplify Failed/Success

--- play-akka-sample-multidc-scala/src/main/scala/sample/model/PaymentLifecycle.scala
+++ play-akka-sample-multidc-scala/src/main/scala/sample/model/PaymentLifecycle.scala
@@ -29,38 +29,35 @@ object PaymentLifecycle {
 
 
     sealed trait Event extends Correlated
-    sealed trait FailedEvent[T <: Command] extends Event
-    sealed trait SuccessfulEvent[T <: Balance] extends Event {
-        def balance: T
-    }
+
     sealed trait RaceConditionEvent[T <: Correlated] extends Event
 
     case class State(history: List[Event])
 
     sealed trait Requested[T <: Command]  extends Event           { def command: T ; val id = command.id }
-    sealed trait Successful[T <: Balance] extends SuccessfulEvent[T] { def balance: T ; val id = balance.id }
-    sealed trait Failed[T <: Command]     extends FailedEvent[T]     { def message: String}
-    sealed trait Timedout[T <: Command]   extends FailedEvent[T]
+    sealed trait Successful[T <: Balance] extends Event { def balance: T ; val id = balance.id }
+    sealed trait Failed                   extends Event     { def message: String}
+    sealed trait Timedout                 extends Failed
 
     case class AuthorizationRequested (command: Authorize) extends Requested[Authorize]
     case class AuthorizationSuccessful(balance: Authorization) extends Successful[Authorization]
-    case class AuthorizationFailed    (id: IdempotentIdentifier, message: String) extends Failed[Authorize]
-    case class AuthorizationTimedout  (id: IdempotentIdentifier, message: String) extends Timedout[Authorize]
+    case class AuthorizationFailed    (id: IdempotentIdentifier, message: String) extends Failed
+    case class AuthorizationTimedout  (id: IdempotentIdentifier, message: String) extends Timedout
 
     case class SettlementRequested (command: Settle) extends Requested[Settle]
     case class SettlementSuccessful(balance: Settlement) extends Successful[Settlement]
-    case class SettlementFailed    (id: IdempotentIdentifier, message: String) extends Failed[Settle]
-    case class SettlementTimeout   (id: IdempotentIdentifier, message: String) extends Timedout[Settle]
+    case class SettlementFailed    (id: IdempotentIdentifier, message: String) extends Failed
+    case class SettlementTimeout   (id: IdempotentIdentifier, message: String) extends Timedout
 
     case class RefundRequested (command: Refund) extends Requested[Refund]
     case class RefundSuccessful(balance: Refunded) extends Successful[Refunded]
-    case class RefundFailed    (id: IdempotentIdentifier, message: String) extends Failed[Refund]
-    case class RefundTimeout   (id: IdempotentIdentifier, message: String) extends Timedout[Refund]
+    case class RefundFailed    (id: IdempotentIdentifier, message: String) extends Failed
+    case class RefundTimeout   (id: IdempotentIdentifier, message: String) extends Timedout
 
     case class ChargebackRequested (command: Chargeback) extends Requested[Chargeback]
     case class ChargebackSuccessful(balance: Chargedback) extends Successful[Chargedback]
-    case class ChargebackFailed    (id: IdempotentIdentifier, message: String) extends Failed[Chargeback]
-    case class ChargebackTimeout   (id: IdempotentIdentifier, message: String) extends Timedout[Chargeback]
+    case class ChargebackFailed    (id: IdempotentIdentifier, message: String) extends Failed
+    case class ChargebackTimeout   (id: IdempotentIdentifier, message: String) extends Timedout
 
     implicit val authorizeReads = Json.reads[Authorize]
     implicit val settleReads = Json.reads[Settle]

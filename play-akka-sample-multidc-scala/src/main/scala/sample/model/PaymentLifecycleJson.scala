46b445341a7388822cc93adf9d60479e3af2c2dd
Sam Schneider <sam.schneider@lightbend.com>
2020-01-07 17:20:03 -0800

    move json formats to rest api / interface

--- play-akka-sample-multidc-scala/src/main/scala/sample/model/PaymentLifecycleJson.scala
+++ play-akka-sample-multidc-scala/src/main/scala/sample/model/PaymentLifecycleJson.scala
@@ -6,7 +6,7 @@ import sample.model.PaymentLifecycle.{Authorize, Chargeback, Refund, Settle}
 object PaymentLifecycleJson {
   // TODO: all of these should move to the play GatewayInterface
 //  implicit val authorizeReads = Json.reads[Authorize]
-  implicit val settleReads = Json.reads[Settle]
-  implicit val refundReads = Json.reads[Refund]
-  implicit val chargebackReads = Json.reads[Chargeback]
+//  implicit val settleReads = Json.reads[Settle]
+//  implicit val refundReads = Json.reads[Refund]
+//  implicit val chargebackReads = Json.reads[Chargeback]
 }
